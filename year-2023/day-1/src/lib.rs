f51ad6df9d28683fbc72e571458e6ed447fddeed
Stefan Breetveld <spiderbiggen@gmail.com>
2023-12-01 14:26:37 +0100

    solutions for 2023 day 1

---
+++ year-2023/day-1/src/lib.rs
@@ -0,0 +1,97 @@
+const INPUT: &str = include_str!("../input_part_1");
+
+mod part_1 {
+    fn process_input(input: &str) -> usize {
+        input
+            .lines()
+            .map(|line| {
+                let chars = line.chars();
+                let mut first_n: Option<usize> = None;
+                let mut last_n: Option<usize> = None;
+                for x in chars {
+                    if let Some(digit) = x.to_digit(10) {
+                        first_n.get_or_insert(digit as usize);
+                        last_n.replace(digit as usize);
+                    }
+                }
+                first_n.unwrap() * 10 + last_n.unwrap()
+            })
+            .fold(0, |acc, x| acc + x)
+    }
+
+    #[test]
+    fn test_process_example() {
+        let example = r#"1abc2
+pqr3stu8vwx
+a1b2c3d4e5f
+treb7uchet"#;
+        assert_eq!(process_input(example), 142);
+    }
+
+    #[test]
+    fn test_process_input() {
+        assert_eq!(process_input(super::INPUT), 55607);
+    }
+}
+
+mod part_2 {
+    fn process_input(input: &str) -> usize {
+        input
+            .lines()
+            .map(|s| process_line(s))
+            .fold(0, |acc, x| acc + x)
+    }
+
+    fn process_line(line: &str) -> usize {
+        // assuming ascii is probably fine for aoc
+        let ascii = line.as_bytes();
+
+        let mut first_n: Option<usize> = None;
+        let mut last_n: Option<usize> = None;
+        for i in 0..ascii.len() {
+            if ascii[i].is_ascii_digit() {
+                let digit = (ascii[i] - b'0') as usize;
+                first_n.get_or_insert(digit);
+                last_n.replace(digit);
+                continue
+            }
+
+            for j in i..ascii.len() {
+                if ascii[j].is_ascii_digit() { break }
+                let num: usize = match &ascii[i..=j] {
+                    s if s.starts_with(b"one") => 1,
+                    s if s.starts_with(b"two") => 2,
+                    s if s.starts_with(b"three") => 3,
+                    s if s.starts_with(b"four") => 4,
+                    s if s.starts_with(b"five") => 5,
+                    s if s.starts_with(b"six") => 6,
+                    s if s.starts_with(b"seven") => 7,
+                    s if s.starts_with(b"eight") => 8,
+                    s if s.starts_with(b"nine") => 9,
+                    _ => continue
+                };
+                first_n.get_or_insert(num);
+                last_n.replace(num);
+                break
+            }
+        }
+        first_n.unwrap() * 10 + last_n.unwrap()
+    }
+
+    #[test]
+    fn test_process_example() {
+        let example = r#"two1nine
+eightwothree
+abcone2threexyz
+xtwone3four
+4nineeightseven2
+zoneight234
+7pqrstsixteen"#;
+        assert_eq!(process_input(example), 281);
+    }
+
+    #[test]
+    fn test_process_input() {
+        assert_eq!(process_input(super::INPUT), 55291);
+    }
+}
